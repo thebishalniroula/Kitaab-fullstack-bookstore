6c6ade7f279e812928108b209e3f35bd2e56e7f0
thebishalniroula <niroulabishal199@gmail.com>
2023-06-08 20:39:25 +0545

    final commit

--- server/routes/auth/admin/register.js
+++ server/routes/auth/admin/register.js
@@ -1,31 +1,33 @@
-const Admin = require("../../../models/Admin");
-const bcrypt = require("bcrypt");
-const inquirer = require("inquirer");
-const mongoose = require("mongoose");
+const Admin = require('../../../models/Admin');
+const bcrypt = require('bcrypt');
+const inquirer = require('inquirer');
+const mongoose = require('mongoose');
 
-require("dotenv").config({ path: "../../../.env" });
-
-mongoose.connect("mongodb://127.0.0.1:27017/newappusers", (err) => {
-  if (err) {
-    return console.log(err);
+require('dotenv').config({ path: '../../../.env' });
+mongoose.connect(
+  'mongodb+srv://bishal:OiPtAP9WytsGY42e@cluster0.klnbx.mongodb.net/?retryWrites=true&w=majority',
+  (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Connected to db');
   }
-  console.log("Connected to db");
-});
+);
 const questions = [
   {
-    type: "input",
-    name: "name",
-    message: "Enter admin name.",
+    type: 'input',
+    name: 'name',
+    message: 'Enter admin name.',
   },
   {
-    type: "input",
-    name: "email",
-    message: "Enter admin email.",
+    type: 'input',
+    name: 'email',
+    message: 'Enter admin email.',
   },
   {
-    type: "input",
-    name: "password",
-    message: "Enter admin password.",
+    type: 'input',
+    name: 'password',
+    message: 'Enter admin password.',
   },
 ];
 
@@ -34,14 +36,14 @@ const createADmin = async (name, email, plainPassword) => {
   const password = await bcrypt.hash(plainPassword, 10);
   const adminDB = await Admin.findOne({ email });
   if (adminDB) {
-    console.log("Admin already registered");
+    console.log('Admin already registered');
   }
 
   const admin = new Admin({ name, email, password });
   try {
     const newAdmin = await admin.save();
     if (newAdmin) {
-      console.log("New admin registered with following details.");
+      console.log('New admin registered with following details.');
       console.log(newAdmin);
     }
   } catch (error) {
@@ -49,7 +51,8 @@ const createADmin = async (name, email, plainPassword) => {
   }
 };
 // Reading values from terminal
-inquirer.prompt(questions).then(({ name, email, password }) => {
-  console.log("name", name);
-  createADmin(name, email, password);
-});
+createADmin('Bishal Niroula', 'niroulabishal199@gmail.com', 'password');
+// inquirer.prompt(questions).then(({ name, email, password }) => {
+// // console.log("name", name);
+//   createADmin(name, email, password);
+// });
